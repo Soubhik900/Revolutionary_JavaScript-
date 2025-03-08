e9205a88aee426a17f6fed82da9ec3e987732262
Soubhik Roy <128206508+Soubhik900@users.noreply.github.com>
2025-03-08 16:10:01 +0000

    Datatypes

--- text.js
+++ text.js
@@ -1,1 +1,34 @@
-console.log("Soubhik")
+/*const accountId=1443;
+let accounEmail="sdjhoco@kcha";
+var accountPassword="746216";
+accountCity="Jaipur";
+let account;
+
+//accountId=2;
+accountPassword="32468";
+accounEmail="jvskjbkbv@NKVBSK";
+accountCity="Bengaluru";
+
+
+console.table([accounEmail,accountId,accountPassword,accountCity,account]);*/
+
+
+let name="Soubhik";
+let age=7;
+let isLoggedIn=false;
+let state=null;
+
+console.log(typeof age);
+
+//number=>2 to the power 53 
+//string
+//null
+//object=> null is object 
+//undefined
+//bigint
+//symbol=> unique
+console.log(typeof undefined);
+console.log(typeof null);
+
+
+
