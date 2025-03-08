5fa3eceea521c4e8c64b6f8c4afb047abf3f0497
Soubhik Roy <128206508+Soubhik900@users.noreply.github.com>
2025-03-08 16:25:18 +0000

    datatypes

---
+++ datatype.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as newer version js
+// cant use no strict after this 
+
+//alert(): we are using node js, not browser
+
+let name="Soubhik";
+let age=7;
+let isLoggedIn=false;
+let state=null;
+
+console.log(typeof age);
+
+//number=>2 to the power 53 
+//string
+//null
+//object=> null is object 
+//undefined
+//bigint
+//symbol=> unique
+console.log(typeof undefined);
+console.log(typeof null);
