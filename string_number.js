c98f297e63e8e7b9a33604fe062f80b59fbc0648
Soubhik Roy <128206508+Soubhik900@users.noreply.github.com>
2025-03-10 07:19:45 +0000

    String number confusion

---
+++ string_number.js
@@ -0,0 +1,15 @@
+/*
+console.log("3"+2+2);
+
+console.log(2+2+"3");
+
+console.log("3"+2);
+
+console.log(2+"3");
+
+//always use correct ways even if it is possible through incorrect ways - for user readability
+*/
+
+
+
+
