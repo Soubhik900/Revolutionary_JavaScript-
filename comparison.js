b10634b10f85b012ec603daf29e765490d51a098
Soubhik Roy <128206508+Soubhik900@users.noreply.github.com>
2025-03-10 08:12:26 +0000

    Comparison

---
+++ comparison.js
@@ -0,0 +1,29 @@
+
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1);
+
+console.log("2">1);//converted string to number and then checks
+
+
+console.log(null>0);//false
+console.log(null==0);//false
+console.log(null>=0);//converts null to number and checks;i.e.; null->0                                                                                           
+
+
+
+//false in all cases
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+
+console.log("2"===2);//=== checks value and datatype, == checks value only
+
+
+
+
+
+
+
