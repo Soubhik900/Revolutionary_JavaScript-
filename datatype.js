e2cee6aef8d7e83c9ce3aa18554ac967aed6ef8f
Soubhik Roy <128206508+Soubhik900@users.noreply.github.com>
2025-03-16 20:17:04 +0530

    Update datatype.js

--- datatype.js
+++ datatype.js
@@ -1,15 +1,21 @@
 "use strict"; //treat all js code as newer version js
 // cant use no strict after this 
 
+// Js is a dynamic language,i.e., variables can hold values of different types during runtime.
+
 //alert(): we are using node js, not browser
 
 let name="Soubhik";
 let age=7;
 let isLoggedIn=false;
 let state=null;
+const id = Symbol('123') // every symbol is unique
+const bigNumber = 7592732244n;
 
 console.log(typeof age);
 
+// datatypes - Primitive and Non-primitive
+//Primitive(value):-
 //number=>2 to the power 53 
 //string
 //null
@@ -17,5 +23,19 @@ console.log(typeof age);
 //undefined
 //bigint
 //symbol=> unique
+//BigInt
 console.log(typeof undefined);
 console.log(typeof null);
+
+//Non-Primitive(reference):-
+//Arrays
+let arr=['A','B','C']; //datatype is object
+//Objects
+let obj={
+  name:'Soubhik',
+  age: 21;
+}
+//Function
+let myFunction=Function(){
+  console.log('Hello World');
+}// datatype is function object
