b16da7fe9bf9970b076d2224f571dbfc4962e5a3
Soubhik Roy <128206508+Soubhik900@users.noreply.github.com>
2025-03-16 15:26:13 +0000

    Stack and Heap

---
+++ stack_heap.js
@@ -0,0 +1,13 @@
+//Stack for primitive datatype
+//Heap for non-primitive datatype
+
+let myName="Soubhik";
+let anotherName=myName;
+anotherName="Jeet";
+
+console.log(myName);
+console.log(anotherName);
+
+
+
+
