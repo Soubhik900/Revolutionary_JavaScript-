768ef87f846dbc040356523a8a47e223560de592
Soubhik Roy <128206508+Soubhik900@users.noreply.github.com>
2025-03-16 17:06:35 +0000

    Update Stack and Heap

--- stack_heap.js
+++ stack_heap.js
@@ -1,6 +1,7 @@
-//Stack for primitive datatype
-//Heap for non-primitive datatype
+//Memory types
 
+//Stack for primitive datatype
+//original value was copied and used, hence no change in original value
 let myName="Soubhik";
 let anotherName=myName;
 anotherName="Jeet";
@@ -8,6 +9,20 @@ anotherName="Jeet";
 console.log(myName);
 console.log(anotherName);
 
+
+//Heap for non-primitive datatype
+//original value was referenced, hence change in original value occurs
+let user1={
+    email: "hr@gmail.com",
+    upi: "hr@ptym"
+
+}
+
+let user2=user1;
+user2.email="pq@gmail.com"
+
+console.log(user1.email);
+console.log(user2.email);
 
 
 
